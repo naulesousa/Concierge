faf66928d92860e7084fec88ddedd2662ea51f87
Naule Sousa <davenstall@gmail.com>
2023-03-01 20:48:11 -0300

    login activity.kt

---
+++ app/src/main/java/com/davenstall/concierge/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.davenstall.concierge.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.davenstall.concierge.data.LoginDataSource
+import com.davenstall.concierge.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
